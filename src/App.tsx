845e456c13e58c4de5b21e8a841546c684600f1b
hectorchan <hectorchanht@gmail.com>
2022-02-14 21:02:41 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,28 @@
-import * as React from "react"
 import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
+  ChakraProvider, Container, extendTheme
 } from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+import * as React from "react"
+import Auth from "./Components/Auth"
+import HistoricalData from "./Components/HistoricalData"
+
+
+const config = {
+  initialColorMode: "dark",
+  useSystemColorMode: true,
+}
+
+const theme = extendTheme({ config })
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
-    </Box>
+    <Container py={6} textAlign="center" fontSize="xl">
+      {/* <ColorModeSwitcher /> */}
+
+      <Auth />
+      <br />
+      <br />
+      <HistoricalData />
+
+    </Container>
   </ChakraProvider>
 )
